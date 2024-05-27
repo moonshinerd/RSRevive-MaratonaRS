b8f743c5b9baa63b4ce4dc022e87352ea46d97dc
moonshinerd <vitorhls15@gmail.com>
2024-05-26 23:38:24 -0300

    criadno a page about us e criando uma rota até ela

---
+++ web/src/pages/aboutus/index.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import styles from './styles.module.css'; // Importando o CSS
+import { Header } from '../../components/header';
+import { Footer } from '../../components/footer';
+
+export const AboutUs = () => {
+  return (
+    <div> 
+      <Header /> 
+      <Footer></Footer>
+    </div>
+  );
+};
